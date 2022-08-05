202498ccff4a4a84b1618e34fc56f8b2defb6415
jose sanchez <jose.sanchez.artist@gmail.com>
2022-08-05 08:43:37 -0700

    initial commit

---
+++ variables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string dogsNames= "Mercury";
+            int dogsAge = 5;
+            char grade= 'A';
+            bool dogGender = true;
+            decimal worldPopulation = 6000000000m;
+            Console.WriteLine($"I have a dog named {dogsNames}.");
+            Console.WriteLine($" She is {dogsAge} years old.");
+            Console.WriteLine($"She would get an {grade} in doggy daycare.");
+            Console.WriteLine($"Yes, it is {dogGender} she is female. She is one of {worldPopulation} in the dog population but she is very special!");       
+        }
+    }
+}
